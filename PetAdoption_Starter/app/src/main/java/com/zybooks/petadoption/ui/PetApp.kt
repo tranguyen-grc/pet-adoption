d947f4806d266f1d5a5d2a24941c76c910eb2b93
Tra Nguyen <tra.nguyen@gmail.com>
2025-10-21 13:40:27 -0700

    finished 7.1

--- PetAdoption_Starter/app/src/main/java/com/zybooks/petadoption/ui/PetApp.kt
+++ PetAdoption_Starter/app/src/main/java/com/zybooks/petadoption/ui/PetApp.kt
@@ -1,13 +1,40 @@
 package com.zybooks.petadoption.ui
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Share
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.zybooks.petadoption.data.Pet
+import com.zybooks.petadoption.data.PetDataSource
+import com.zybooks.petadoption.data.PetGender
+import com.zybooks.petadoption.ui.theme.PetAdoptionTheme
 
 @Composable
 fun PetApp(
@@ -30,4 +57,170 @@ fun PetAppBar(
       ),
       modifier = modifier
    )
+}
+
+@Composable
+fun ListScreen(
+   petList: List<Pet>,
+   onImageClick: (Pet) -> Unit,
+   modifier: Modifier = Modifier
+) {
+   Scaffold(
+      topBar = {
+         PetAppBar(
+            title = "Find a Friend"
+         )
+      }
+   ) { innerPadding ->
+      LazyVerticalGrid(
+         columns = GridCells.Adaptive(minSize = 128.dp),
+         contentPadding = PaddingValues(0.dp),
+         modifier = modifier.padding(innerPadding)
+      ) {
+         items(petList) { pet ->
+            Image(
+               painter = painterResource(id = pet.imageId),
+               contentDescription = "${pet.type} ${pet.gender}",
+               modifier = Modifier.clickable(
+                  onClick = { onImageClick(pet) },
+                  onClickLabel = "Select the pet"
+               )
+            )
+         }
+      }
+   }
+}
+
+@Preview
+@Composable
+fun PreviewListScreen() {
+   PetAdoptionTheme {
+      ListScreen(
+         petList = PetDataSource().loadPets(),
+         onImageClick = { }
+      )
+   }
+}
+
+@Composable
+fun DetailScreen(
+   pet: Pet,
+   onAdoptClick: () -> Unit,
+   modifier: Modifier = Modifier,
+   onUpClick: () -> Unit = { }
+) {
+   val gender = if (pet.gender == PetGender.MALE) "Male" else "Female"
+
+   Scaffold(
+      topBar = {
+         PetAppBar(
+            title = "Details",
+         )
+      }
+   ) { innerPadding ->
+      Column(
+         modifier = modifier.padding(innerPadding)
+      ) {
+         Image(
+            painter = painterResource(pet.imageId),
+            contentDescription = pet.name,
+            contentScale = ContentScale.FillWidth
+         )
+         Column(
+            verticalArrangement = Arrangement.spacedBy(6.dp),
+            modifier = modifier.padding(6.dp)
+         ) {
+            Row(
+               horizontalArrangement = Arrangement.SpaceBetween,
+               verticalAlignment = Alignment.CenterVertically,
+               modifier = modifier.fillMaxWidth()
+            ) {
+               Text(
+                  text = pet.name,
+                  style = MaterialTheme.typography.headlineMedium
+               )
+               Button(onClick = onAdoptClick) {
+                  Text("Adopt Me!")
+               }
+            }
+            Text(
+               text = "Gender: $gender",
+               style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+               text = "Age: ${pet.age}",
+               style = MaterialTheme.typography.bodyLarge
+            )
+            Text(
+               text = pet.description,
+               style = MaterialTheme.typography.bodyMedium
+            )
+         }
+      }
+   }
+}
+
+@Preview
+@Composable
+fun PreviewDetailScreen() {
+   val pet = PetDataSource().loadPets()[0]
+   PetAdoptionTheme {
+      DetailScreen(
+         pet = pet,
+         onAdoptClick = { }
+      )
+   }
+}
+
+@Composable
+fun AdoptScreen(
+   pet: Pet,
+   modifier: Modifier = Modifier,
+   onUpClick: () -> Unit = { }
+) {
+   Scaffold(
+      topBar = {
+         PetAppBar(
+            title = "Thank You!",
+         )
+      }
+   ) { innerPadding ->
+      Column(
+         modifier = modifier.padding(innerPadding)
+      ) {
+         Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(
+               painter = painterResource(pet.imageId),
+               contentDescription = pet.name,
+               modifier = modifier.size(150.dp)
+            )
+            Text(
+               text = "Thank you for adopting ${pet.name}!",
+               modifier = modifier.padding(horizontal = 28.dp),
+               textAlign = TextAlign.Center,
+               style = MaterialTheme.typography.headlineLarge,
+            )
+         }
+         Text(
+            text = "Please pick up your new family member during business hours.",
+            modifier = modifier.padding(6.dp),
+         )
+         Button(
+            onClick = { },
+            modifier = modifier.padding(6.dp)
+         ) {
+            Icon(Icons.Default.Share, null)
+            Text("Share", modifier = modifier.padding(start = 8.dp))
+         }
+      }
+   }
+}
+
+@Preview
+@Composable
+fun PreviewAdoptScreen() {
+   val pet = PetDataSource().loadPets()[0]
+   PetAdoptionTheme {
+      AdoptScreen(pet)
+   }
 }
